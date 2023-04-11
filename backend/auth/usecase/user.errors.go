3a492598152c680cb970064baa9dd636412daf3d
Cleverson <cleversonsoares.zero@gmail.com>
2023-04-11 13:42:59 -0300

    ads

--- backend/auth/usecase/user.errors.go
+++ backend/auth/usecase/user.errors.go
@@ -2,4 +2,5 @@ package usecase
 
 import "errors"
 
+// ErrInvalidEmailOrPassword records an error indicating an invalid email por password provided.
 var ErrInvalidEmailOrPassword = errors.New("invalid email or password")
